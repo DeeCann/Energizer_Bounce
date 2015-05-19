6a48e56c08d7853c1cf0c1c9253c47548f8e460b
Bartek <bartek@lablike.net>
2015-05-19 22:08:32 +0200

    New repo

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+    private bool _setPosition = false;
+    private Vector3 _touchedPosition;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            _setPosition = true;
+
+        if (Input.GetMouseButtonUp(0)) { 
+            BounceObject.Instance.GetComponent<LineRenderer>().SetPosition(1, Vector3.zero);
+            BounceObject.Instance.GetComponent<Rigidbody>().AddForce((_touchedPosition - BounceObject.Instance.transform.position) * 300);
+            _setPosition = false;
+        }
+            
+
+        if (_setPosition)
+        {
+            _touchedPosition = Input.mousePosition;
+            _touchedPosition.z += transform.position.y;
+
+
+            _touchedPosition = GetComponent<Camera>().ScreenToWorldPoint(_touchedPosition);
+
+            BounceObject.Instance.GetComponent<LineRenderer>().SetPosition(0, BounceObject.Instance.transform.position);
+            BounceObject.Instance.GetComponent<LineRenderer>().SetPosition(1, _touchedPosition);
+        }
+
+        RaycastHit hitinfo;
+        if (Physics.Linecast(BounceObject.Instance.transform.position, _touchedPosition, out hitinfo))
+        {
+            Debug.Log(hitinfo.collider.name);
+        }
+
+
+
+	}
+}
---
+++ Assets/Scripts/BounceObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceObject : MonoBehaviour {
+
+	private static BounceObject _instance = null;
+
+    public static BounceObject Instance {
+        get {
+            if (_instance == null)
+                _instance = GameObject.FindObjectOfType(typeof(BounceObject)) as BounceObject;
+
+            return _instance;
+        }
+    }
+
+    void Awake() {
+        _instance = this;
+    }
+}
